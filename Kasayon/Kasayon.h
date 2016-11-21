6a499cd6f995481bf433d520c9c20b258b3c7641
Erik van der Wal <erikvdwal@gmail.com>
2016-11-21 07:11:48 +0100

    Initial Commit

---
+++ Kasayon/Kasayon.h
@@ -0,0 +1,19 @@
+//
+//  Kasayon.h
+//  Kasayon
+//
+//  Created by Erik van der Wal on 21-11-16.
+//  Copyright © 2016 Erik van der Wal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kasayon.
+FOUNDATION_EXPORT double KasayonVersionNumber;
+
+//! Project version string for Kasayon.
+FOUNDATION_EXPORT const unsigned char KasayonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kasayon/PublicHeader.h>
+
+
